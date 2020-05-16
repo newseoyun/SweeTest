edd0e7f3a516c07d2a1711bc2f751c6210371721
newsy lee <newseoyun@gmail.com>
2020-05-16 10:22:21 +0000

    ㄱㄱ

---
+++ nodeJS_Book/Chapter_04/04_02_02.js
@@ -0,0 +1,33 @@
+/*
+--- 응답 헤더 ---
+* ['x-forwarded-for'] 실제 IP를 헤더에 포함시키는 기능. (프록시나 로드밸런서를 통과하면 IP를 알 수 없게 되므로)
+    실제 IP로 로그를 기록하는 등의 활용을 함.
+
+* 접속하면 서버에 두 개의 요청이 들어옴. 메인 & favicon.ico
+    
+
+
+*/
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('------------------ request 정보 ------------------');
+    console.log("클라이언트 ip: ", ip);
+    console.log("url: ", req.url);
+    console.log("headers: ", req.headers);
+    console.log('--------------------------------------------------');
+    
+    // res.statusCode = 200;
+    // res.setHeader('Content-type', 'text/plain');
+    res.writeHead(200, { 'Content-Type' : 'text/plain; charset=utf-8' });
+    res.end('지긋지긋한 헬로월드\n' + JSON.stringify(req.headers, null, 4) + '\n메롱메롱');
+});
+
+server.listen(3000, (err) => { 
+    if (err) { 
+        console.log(err); 
+    }
+    console.log('서버 기동 중'); 
+});
---
+++ nodeJS_Book/Chapter_05/console.js
@@ -0,0 +1,24 @@
+/*
+console엔 log, error, info 등등 많이 있음. 문서 참고(https://nodejs.org/api/console.html)
+*/
+
+// 기본 출력
+let item = 50;
+console.log('Count1: ', item);
+console.log('Count2: %d', item);
+console.log(`Count3: ${item}`);
+
+
+// 예제
+/*
+setInterval(() => {
+    console.log("지금 시간은 ", new Date(), "입니다.");
+}, 5 * 1000);
+*/
+
+// ANSI 패턴으로 예쁘게 써보자 (ANSI 예제: https://sosomemo.tistory.com/59)
+// 크롬에선 옵션(5; 이런것) 안먹히네...
+console.log('ANSI 패턴으로 예쁘게 써보자');
+console.log("\033[36m" + new Date() + "\033[0m"); // \033[ 이건 \e[(알파벳 e) 혹은 \0x1B[(16진수) 이거랑 같다(이스케이프 및 ANSI코드의 시작을 의미함). 36m은 청록색. \033[0m 이건 초기화 의미.
+console.log("\033[5;31m" + new Date() + "\033[0m");
+console.log("\033[31m\033[43m" + new Date() + "\033[0m"); // \033[43m 뒤에 한번 더 오면 배경
