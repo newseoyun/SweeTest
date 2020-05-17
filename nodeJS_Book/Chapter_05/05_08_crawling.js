5a3a49accb3823cea95be8ee5105a8a1c7c704b0
newsy lee <newseoyun@gmail.com>
2020-05-17 08:14:34 +0000

    5

--- nodeJS_Book/Chapter_05/05_08_crawling.js
+++ nodeJS_Book/Chapter_05/05_08_crawling.js
@@ -4,82 +4,88 @@ const https = require('https');
 const url = 'https://nodejs.org/docs/latest-v12.x/api/';
 let crawlData = [];
 
-https.get(url + 'all.json', (res) => {
+https.get(url + 'index.json', (res) => {
     let body = '';
     res.on('data', (d) => {
         body += d;
     });
     
     res.on('end', () => {
-        let indexData = JSON.parse(body).desc;
-        
-        for(let i=0; i<indexData.length; i++){
-            if(indexData[i].type == 'text'){
-                let str = indexData[i].text;
-                str = str.substr(str.indexOf("[") + 1);
-                let tempIdx = str.indexOf("]");
-                let title = str.substr(0, tempIdx);
-                
-                str = str.substr(tempIdx + 1);
-                let link = str.slice(1, -1).replace(".html", ".json");
-                
-                crawlData.push({
-                    'title': title,
-                    'link': link,
-                    'methods': []
-                });
-            }
-        }
-        
-        setTimeout(() => {
-            GetMethod();
-        }, 1000);
+        let indexData = body;
+        console.log('body : ' + indexData);
     });
+    
+    // res.on('end', () => {
+    //     let indexData = JSON.parse(body).desc;
+        
+    //     for(let i=0; i<indexData.length; i++){
+    //         if(indexData[i].type == 'text'){
+    //             let str = indexData[i].text;
+    //             str = str.substr(str.indexOf("[") + 1);
+    //             let tempIdx = str.indexOf("]");
+    //             let title = str.substr(0, tempIdx);
+                
+    //             str = str.substr(tempIdx + 1);
+    //             let link = str.slice(1, -1).replace(".html", ".json");
+                
+    //             crawlData.push({
+    //                 'title': title,
+    //                 'link': link,
+    //                 'methods': []
+    //             });
+    //         }
+    //     }
+        
+    //     setTimeout(() => {
+    //         GetMethod();
+    //     }, 1000);
+    // });
 }).on('error', (e) => {
     console.log("ERROR: " + e);
 });
 
-let pageIdx = 0;
-const GetMethod = () => {
-    console.log("Get Methods");
+
+// let pageIdx = 0;
+// const GetMethod = () => {
+//     console.log("Get Methods");
     
-    https.get(url + crawlData[pageIdx].link, (res) => {
-        let body = '';
-        res.on('data', (d) => {
-            body += d;
-        });
+//     https.get(url + crawlData[pageIdx].link, (res) => {
+//         let body = '';
+//         res.on('data', (d) => {
+//             body += d;
+//         });
         
-        res.on('end', () => {
-            const temp = JSON.parse(body);
+//         res.on('end', () => {
+//             const temp = JSON.parse(body);
             
-            if(!temp || !temp.modules || !temp.modules.length == 0 || !temp.modules[0].methods){
-                pageIdx++;
-                setTimeout(() => {
-                    GetMethod();
-                }, 1000);
-                return;
-            }
-            let indexData = temp.modules[0].methods;
+//             if(!temp || !temp.modules || !temp.modules.length == 0 || !temp.modules[0].methods){
+//                 pageIdx++;
+//                 setTimeout(() => {
+//                     GetMethod();
+//                 }, 1000);
+//                 return;
+//             }
+//             let indexData = temp.modules[0].methods;
             
-            for(let i=0; i<indexData.length; i++){
-                crawlData[pageIdx].methods.push({
-                    textRaw: indexData[i].textRaw,
-                    desc: indexData[i].desc,
-                    signatures: indexData[i].signatures
-                });
-            }
+//             for(let i=0; i<indexData.length; i++){
+//                 crawlData[pageIdx].methods.push({
+//                     textRaw: indexData[i].textRaw,
+//                     desc: indexData[i].desc,
+//                     signatures: indexData[i].signatures
+//                 });
+//             }
             
-            if(pageIdx < crawlData.length-1){
-                pageIdx++;
-                setTimeout(() => {
-                    GetMethod();           
-                }, 1000);
-            }else{
-                console.log(crawlData);
-            }
-        });
-    }).on('error', (e) => {
-        console.log("ERROR: " + e);
-    });
-};
+//             if(pageIdx < crawlData.length-1){
+//                 pageIdx++;
+//                 setTimeout(() => {
+//                     GetMethod();           
+//                 }, 1000);
+//             }else{
+//                 console.log(crawlData);
+//             }
+//         });
+//     }).on('error', (e) => {
+//         console.log("ERROR: " + e);
+//     });
+// };
 
---
+++ nodeJS_Book/Chapter_05/05_08_crawling_my.js
@@ -0,0 +1,50 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+// const url = 'https://n.weather.naver.com/today/09200114';
+// let crawlData = [];
+
+// https.get(url, (res) => {
+//     let body = '';
+//     res.on('data', (d) => {
+//         body += d;
+//     });
+    
+//     res.on('end', () => {
+//         let fullData = body;
+//         let startNum = fullData.indexOf('<!-- 주간예보');
+//         let endNum = fullData.indexOf('<!-- 광고');
+//         let weather = fullData.substring(startNum, endNum);
+        
+//         // 수집한 태그를 파일로 저장할것임
+//         const useStream = fs.createWriteStream('weatherTag.html', {flags: 'w'}); // 핸들을 연다
+
+//         useStream.on('open', () => { 
+//                 console.log("- 파일로 저장 -")
+//             useStream.write(weather);
+//             useStream.end(() => {
+//                 console.log("- 저장 완료 -");
+//             });
+//         });
+        
+//         console.log("startNum: "+startNum, " | endNum: "+endNum);
+
+//     });
+// }).on('error', (e) => {
+//     console.log("ERROR: " + e);
+// });
+
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
+    fs.readFile('weatherTag.html', (err, data) => {
+        if(err) throw err;
+        res.end(data);
+    })
+}).listen(3000, (err) => { 
+    if (err) { 
+        console.log(err); 
+    }
+    console.log('서버 기동 중'); 
+});
